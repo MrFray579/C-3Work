8e357945bdcacd0fa205fbef18d345606806bdf5
Fray <kozar20000@mail.ru>
2022-06-29 20:19:54 +0300

    array

---
+++ Array/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[a];
+
+for ( int i = 0; i < a; i++)
+{
+    array[i] = i;
+    Console.WriteLine(array[i]);
+}
+
+
+
