6ffd6a12cb974ce42632d129efa1be9944c1d9fd
Fray <kozar20000@mail.ru>
2022-06-29 20:03:27 +0300

    Remainder2

---
+++ Remainder2/Program.cs
@@ -0,0 +1,13 @@
+﻿void remainder (int a)
+{
+    if (a % 7 == 0 & a % 23 == 0)
+    {
+        Console.WriteLine("Число кратно");
+    }
+    else
+    {
+        Console.WriteLine("Число не кратно " + a);
+    }
+}
+
+remainder (161);
