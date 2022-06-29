363dcab1f4599ec7f6eb5044be398732e23175c6
Fray <kozar20000@mail.ru>
2022-06-29 19:31:03 +0300

    Randomnum

---
+++ Randomnum/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int something (int start, int finish)
+{
+    int A = new Random().Next(start, finish);
+    Console.WriteLine(A);
+
+    int thirdnum = A % 10;
+    int firstnum = A / 100;
+    int result = firstnum * 10 + thirdnum;
+    return result;
+}
+
+int a = something (100, 999);
+Console.WriteLine(a);
+
