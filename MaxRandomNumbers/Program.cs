25b7d7dc77b77d116e6b13807726438727b7ec1b
Fray <kozar20000@mail.ru>
2022-06-29 19:09:05 +0300

    MaxRandomNumbers

---
+++ MaxRandomNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿void compareNumbers (int firstA, int secondA)
+{
+    if (secondA > firstA)
+    {
+        Console.WriteLine(secondA + " больше " + firstA);
+    }
+    else
+    {
+        Console.WriteLine(firstA + " больше " + secondA);
+    }
+}
+
+
+
+void randomNumbersCompare (int start, int finish)
+{
+    int A = new Random().Next(start, finish);
+    Console.WriteLine(A);
+    int secondA = A % 10;
+    int firstA = A / 10;
+    compareNumbers(firstA, secondA);
+}
+
+randomNumbersCompare (10, 99);
+
