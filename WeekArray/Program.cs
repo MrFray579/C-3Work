596bee0d377dddc5f496ff2106806108509177fb
Fray <kozar20000@mail.ru>
2022-06-29 20:33:38 +0300

    weekDays

---
+++ WeekArray/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int a = Convert.ToInt32(Console.ReadLine());
+string [] weekDays = new string[] {"Sun", "Mon", "Tue", "Web", "Thu", "Fri", "Sat"};
+
+if (a <= 7)
+Console.WriteLine(weekDays[a - 1]);
+else
+Console.WriteLine("Введите другое число");
