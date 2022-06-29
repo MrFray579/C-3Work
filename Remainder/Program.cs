d14450bcd22f63101c9962ac25f22e57c9a73858
Fray <kozar20000@mail.ru>
2022-06-29 19:56:47 +0300

    remaiber

---
+++ Remainder/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+    Console.WriteLine("Число " + a + " кратно числу " + b);
+else
+    Console.WriteLine("Число " + a + " не кратно числу " + b + "." + " Остаток " + a % b);*/
+
+void remainder (int a, int b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine("Число " + a + " кратно числу " + b);
+    }
+    else
+    {
+        Console.WriteLine("Число " + a + " не кратно числу " + b + "." + " Остаток " + a % b);
+    }
+}
+
+remainder (75, 16);
